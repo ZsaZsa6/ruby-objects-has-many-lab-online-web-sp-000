b7eaf05e16594e8e8b1a07bb944593233ba39490
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-23 23:45:03 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,13 +6,13 @@ class Artist
     @songs = []
   end
 
-  def songs
-    @songs
-  end
-
   def add_song(song)
   @songs << song
   song.artist = self
+  end
+
+  def songs
+    @songs
   end
 
   def add_song_by_name(song)
