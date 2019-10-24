21502f24e96001cc57d99b54e9614117b5fe3e64
Ronda D Cowen <cowenhome5@gmail.com>
2019-10-24 01:54:42 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,25 @@
 class Post
   attr_accessor :title, :author 
     @@all= []
+    
   def initialize(title)
    @title = title
-   @@all << self
+   save
+  end
+  
+  def title
+    @title
   end
 
+  def save
+    @@all << self
+  end
+  
   def self.all
    @@all
   end
+  
   def author_name
-    @author
+    self.author.name if author
   end
-
 end
